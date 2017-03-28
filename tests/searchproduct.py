920df1bb3e05510e206c769dd51328aec6d49505
Edwin Marte Torres <kdgpro15@gmail.com>
2017-03-28 18:43:39 -0400

    Implementing Unittest

--- tests/searchproduct.py
+++ tests/searchproduct.py
@@ -1,43 +1,58 @@
+import os
 import time
+import unittest
 from selenium import webdriver
 
-# Create new Session
-driver = webdriver.Firefox(executable_path='./drivers/geckodriver.exe')
-driver.implicitly_wait(30)
-driver.maximize_window()
 
-# Navigate to the application homepage
-driver.get("http://demo.magentocommerce.com/")
+class SearchTest(unittest.TestCase):
+    def setUp(self):
+        drivers_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'drivers'))
 
-# Click on Search Button
-search_button = driver.find_element_by_class_name("search")
-search_button.click()
+        # Select the Driver
+        driver_selection = input("Which browser you need to test with? (ch, ff, ie)? ")
 
-# Get the search textbox
-search_field = driver.find_element_by_name("keys")
-search_field.clear()
+        if driver_selection == "ff":
+            self.driver = webdriver.Firefox(executable_path='%s\%s' % (drivers_folder, 'geckodriver.exe'))
+        elif driver_selection == "ie":
+            self.driver = webdriver.Ie(executable_path='%s\%s' % (drivers_folder, 'iedriverserver.exe'))
+        else:
+            self.driver = webdriver.Chrome(executable_path='%s\%s' % (drivers_folder, 'chromedriver.exe'))
 
-# Enter search keyword and Submit
-search_field.send_keys("phones")
-search_field.submit()
+        # Create new Session
+        self.driver.implicitly_wait(30)
+        self.driver.maximize_window()
 
-# Get Products Link
-products_link = driver.find_element_by_link_text("products")
-products_link.click()
+        # Navigate to the application homepage
+        self.driver.get("http://demo.magentocommerce.com/")
 
-# Wait until page reloads javascript || jQuery
-time.sleep(5)
+    def test_search_by_category(self):
+        # Click on Search Button
+        self.search_button = self.driver.find_element_by_class_name("search")
+        self.search_button.click()
 
-# Get all the anchor elements which have product names displayed currently on result page using
-# xpath method
-products = driver.find_elements_by_xpath("//div[@class='result-title']/a")
+        # Get the search textbox
+        self.search_field = self.driver.find_element_by_name("keys")
+        self.search_field.clear()
 
-# Get the number of anchor elements found
-print("Found %s products: " % str(len(products)))
+        # Enter search keyword and Submit
+        self.search_field.send_keys("phones")
+        self.search_field.submit()
 
-# Iterate through each anchor element and print the text that is name of the product
-for product in products:
-    print(product.text)
+        # Get Products Link
+        self.products_link = self.driver.find_element_by_link_text("products")
+        self.products_link.click()
 
-# Close the browser window
-driver.quit()
+        # Wait until page reloads javascript || jQuery
+        time.sleep(5)
+
+        # Get all the anchor elements which have product names displayed currently on result page using
+        # xpath method
+        products = self.driver.find_elements_by_xpath("//div[@class='result-title']/a")
+        self.assertEqual(1, len(products))
+
+    def tearDown(self):
+        # Close the browser window
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
