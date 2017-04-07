8dcf4dd19ee1dcadf7acdebaed5246b215ba52b1
Edwin Marte Torres <kdgpro15@gmail.com>
2017-04-06 21:29:37 -0400

    Chapter 2 - Homepage Test

---
+++ tests/homepage.py
@@ -0,0 +1,74 @@
+import os
+import time
+import unittest
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+
+class HomePageTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Create a new session
+        drivers_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'drivers'))
+
+        # Select the Driver
+        driver_selection = input("Which browser you need to test with? (ch, ff, ie)? ")
+
+        if driver_selection == "ff":
+            cls.driver = webdriver.Firefox(executable_path='%s\%s' % (drivers_folder, 'geckodriver.exe'))
+        elif driver_selection == "ie":
+            cls.driver = webdriver.Ie(executable_path='%s\%s' % (drivers_folder, 'iedriverserver.exe'))
+        else:
+            cls.driver = webdriver.Chrome(executable_path='%s\%s' % (drivers_folder, 'chromedriver.exe'))
+
+        # Create new Session
+        cls.driver.implicitly_wait(30)
+        cls.driver.maximize_window()
+
+        # Navigate to the application homepage
+        cls.driver.get("https://www.ebay.com/")
+        cls.title = cls.driver.title
+
+    def test_search_field(self):
+        """ Locating Search Field """
+        # Check search field exists on HomePage
+        self.assertTrue(self.is_element_present(By.NAME, "_nkw"))
+
+    def test_language_option(self):
+        """ Locating Language Option """
+        # Check language options dropdown on Homepage
+        self.assertTrue(self.is_element_present(By.ID, "gh-eb-Geo"))
+
+    def test_shopping_cart_empty_message(self):
+        """ Checking Shopping Cart is Empty """
+        # Check content of My Shopping Cart block on Homepage
+        shopping_cart_icon = self.driver.find_element_by_css_selector("li#gh-cart")
+        shopping_cart_icon.click()
+
+        shopping_cart_status = self.driver.find_element_by_css_selector("div.fw-b").text
+        self.assertEqual("Tu carro de compras está vacío, pero no tiene por qué estarlo.", shopping_cart_status)
+
+        # close_button = self.driver.find_element_by_css_selector("a#msgClose")
+        # close_button.click()
+        # time.sleep(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        # Close the browser window
+        cls.driver.quit()
+
+    def is_element_present(self, how, what):
+        """
+        Utility method to check presence of an element on page
+        :params how: By locator type
+        :params what: locator value
+        """
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException:
+            return False
+        return True
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
--- tests/search_product.py
+++ tests/search_product.py
@@ -5,25 +5,27 @@ from selenium import webdriver
 
 
 class SearchTest(unittest.TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
         drivers_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'drivers'))
 
         # Select the Driver
         driver_selection = input("Which browser you need to test with? (ch, ff, ie)? ")
 
         if driver_selection == "ff":
-            self.driver = webdriver.Firefox(executable_path='%s\%s' % (drivers_folder, 'geckodriver.exe'))
+            cls.driver = webdriver.Firefox(executable_path='%s\%s' % (drivers_folder, 'geckodriver.exe'))
         elif driver_selection == "ie":
-            self.driver = webdriver.Ie(executable_path='%s\%s' % (drivers_folder, 'iedriverserver.exe'))
+            cls.driver = webdriver.Ie(executable_path='%s\%s' % (drivers_folder, 'iedriverserver.exe'))
         else:
-            self.driver = webdriver.Chrome(executable_path='%s\%s' % (drivers_folder, 'chromedriver.exe'))
+            cls.driver = webdriver.Chrome(executable_path='%s\%s' % (drivers_folder, 'chromedriver.exe'))
 
         # Create new Session
-        self.driver.implicitly_wait(30)
-        self.driver.maximize_window()
+        cls.driver.implicitly_wait(30)
+        cls.driver.maximize_window()
 
         # Navigate to the application homepage
-        self.driver.get("http://demo.magentocommerce.com/")
+        cls.driver.get("http://demo.magentocommerce.com/")
+        cls.title = cls.driver.title
 
     def test_search_by_category(self):
         # Click on Search Button
@@ -37,12 +39,32 @@ class SearchTest(unittest.TestCase):
         # Enter search keyword and Submit
         self.search_field.send_keys("phones")
         self.search_field.submit()
+        time.sleep(5)
 
         # Get Products Link
         self.products_link = self.driver.find_element_by_link_text("products")
         self.products_link.click()
+        time.sleep(5)
 
-        # Wait until page reloads javascript || jQuery
+        # Get all the anchor elements which have product names displayed currently on result page using
+        # xpath method
+        products = self.driver.find_elements_by_xpath("//div[@class='result-title']/a")
+        self.assertEqual(1, len(products))
+        self.assertNotEqual(products[0].text, "")
+
+    def test_search_by_name(self):
+        # Get the search textbox
+        self.search_field = self.driver.find_element_by_name("keys")
+        self.search_field.clear()
+
+        # Enter search keyword and Submit
+        self.search_field.send_keys("salt shaker")
+        self.search_field.submit()
+        time.sleep(5)
+
+        # Get Products Link
+        self.products_link = self.driver.find_element_by_link_text("products")
+        self.products_link.click()
         time.sleep(5)
 
         # Get all the anchor elements which have product names displayed currently on result page using
@@ -50,9 +72,11 @@ class SearchTest(unittest.TestCase):
         products = self.driver.find_elements_by_xpath("//div[@class='result-title']/a")
         self.assertEqual(1, len(products))
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(cls):
         # Close the browser window
-        self.driver.quit()
+        cls.driver.quit()
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
